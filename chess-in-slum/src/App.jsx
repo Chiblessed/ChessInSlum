8ac306e3fdc62241df1e257fb934f89301e87346
Chisom Ohanu <148290753+Chiblessed@users.noreply.github.com>
2024-10-10 12:28:30 +0100

    done

--- chess-in-slum/src/App.jsx
+++ chess-in-slum/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {HashRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
