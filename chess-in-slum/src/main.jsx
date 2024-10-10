8c012c03354232af9ebf60b5724b23ba397c97f7
Chisom Ohanu <148290753+Chiblessed@users.noreply.github.com>
2024-10-10 04:16:43 +0100

    done

--- chess-in-slum/src/main.jsx
+++ chess-in-slum/src/main.jsx
@@ -6,14 +6,6 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-<Router basename='/ChessInSlum/'>
-  <Routes>
-    <Route path='#' element={    <App />}/>
-  </Routes>
-</Router>
- 
-
-
-  </React.StrictMode>,
+<App/>
+  </React.StrictMode>
 )
