d51a4a542621362dc2131bc21fe8d16a54ee2269
Chisom Ohanu <148290753+Chiblessed@users.noreply.github.com>
2024-10-14 15:57:04 +0100

    done

--- chess-in-slum/src/App.jsx
+++ chess-in-slum/src/App.jsx
@@ -2,20 +2,18 @@ import './App.css';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/Footer';
-import { lazy, Suspense } from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Merchandise from './pages/Merchandise';
 
-const Home = lazy(() => import('./pages/Home'));
-const About = lazy(() => import('./pages/About'));
-const Blog = lazy(() => import('./pages/Blog'));
-const Contact = lazy(() => import('./pages/Contact'));
-const Merchandise = lazy(() => import('./pages/Merchandise'));
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Suspense fallback={<div className='text-center mt-52 font-extrabold text-5xl'>Loading...</div>}>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
@@ -23,7 +21,6 @@ function App() {
             <Route path='/contact' element={<Contact />} />
             <Route path='/merchandise' element={<Merchandise />} />
           </Routes>
-        </Suspense>
       </Router>
       <Footer />
     </>
